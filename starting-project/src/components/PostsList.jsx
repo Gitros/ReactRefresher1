259bf66cfea0281b25e39d7a4ba3d0aa7d0a45e5
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2025-10-22 19:11:50 +0200

    adding event listener

--- starting-project/src/components/PostsList.jsx
+++ starting-project/src/components/PostsList.jsx
@@ -1,11 +1,15 @@
 import Post from "./Post";
+import NewPost from "./NewPost";
 import classes from "./PostsList.module.css";
 
 export default function PostList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Maximilian" body="React.js is awesome!" />
-      <Post author="Manuel" body="React.js is awesome lol!" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Maximilian" body="React.js is awesome!" />
+        <Post author="Manuel" body="React.js is awesome lol!" />
+      </ul>
+    </>
   );
 }
