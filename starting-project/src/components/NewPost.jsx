e9f30e1a1efb766b8faee5a32384e5447bbaee99
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2025-10-23 17:58:02 +0200

    updating state based on previous state

--- starting-project/src/components/NewPost.jsx
+++ starting-project/src/components/NewPost.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import classes from "./NewPost.module.css";
 
-export default function NewPost({ onCancel }) {
+export default function NewPost({ onCancel, onAddPost }) {
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
 
@@ -18,6 +18,7 @@ export default function NewPost({ onCancel }) {
       body: enteredBody,
       author: enteredAuthor,
     };
+    onAddPost(postData);
     onCancel();
   }
 
--- starting-project/src/components/PostsList.jsx
+++ starting-project/src/components/PostsList.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import Modal from "./Modal";
 import Post from "./Post";
 import NewPost from "./NewPost";
 import classes from "./PostsList.module.css";
 
 export default function PostList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
+
+  function addPostHandler(postData) {
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+  }
+
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} />
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
 
