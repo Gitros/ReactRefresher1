2ce397f7f3d7cdb907b3d854364e83338486cf82
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2025-10-23 19:47:15 +0200

    dynamic routes

--- starting-project/src/main.jsx
+++ starting-project/src/main.jsx
@@ -4,6 +4,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import NewPost, { action as newPostAction } from "./routes/NewPost";
+import PostDetails, { loader as postDetailsLoader } from "./routes/PostDetails";
 import RootLayout from "./routes/RootLayout";
 import Posts, { loader as postsLoader } from "./routes/Posts";
 
@@ -18,6 +19,7 @@ const router = createBrowserRouter([
         loader: postsLoader,
         children: [
           { path: "/create-post", element: <NewPost />, action: newPostAction },
+          { path: "/:id", element: <PostDetails />, loader: postDetailsLoader },
         ],
       },
     ],
